f8351e1376be0b13fbfe47d157d341105e631dd9
Zubair Shaikh <zsescape@gmail.com>
2019-08-23 02:56:26 -0400

    Module 02

--- CarRO/repair/models.py
+++ CarRO/repair/models.py
@@ -3,22 +3,23 @@ from mongoengine.fields import *
 
 
 class Vehicle(EmbeddedDocument):
-    make = StringField
-    model = StringField
-    license_no = StringField
+    make = StringField()
+    model = StringField()
+    license_no = StringField()
 
 
 class Customer(EmbeddedDocument):
-    name = StringField
-    email_id = EmailField
+    name = StringField()
+    email_id = EmailField()
     mobile_no = StringField(max_length=10)
 
 
-states = ('ORDERED', 'REPAIRING', 'REPAIRED', 'REVIEWING', 'DONE')
+states = ("ORDERED", "REPAIRING", "REPAIRED", "REVIEWING", "DONE")
 
 
 class RepairOrder(Document):
-    id = IntField(primary_key=True, unique=True)
+    id = SequenceField(primary_key=True, unique=True)
     vehicle = EmbeddedDocumentField(Vehicle)
-    Customer = EmbeddedDocumentField(Vehicle)
-    status = StringField(choices=states)
+    customer = EmbeddedDocumentField(Customer)
+    status = StringField(choices=states, default="ORDERED")
+    complaint = StringField(max_length=300)
