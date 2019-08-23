79548ed5f6f6c1532937f7f4e2f0b92bd527ab48
Zubair Shaikh <zsescape@gmail.com>
2019-08-23 02:59:20 -0400

    Module 03

--- CarRO/repair/views.py
+++ CarRO/repair/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework_mongoengine.generics import RetrieveUpdateDestroyAPIView
+from rest_framework_mongoengine.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import RepairOrder
+from .serializers import RepairOrderSerializer
+
+
+class RepairOrderList(ModelViewSet):
+    lookup_field = 'id'
+    serializer_class = RepairOrderSerializer
+    queryset = RepairOrder.objects.all()
+
+    def post(self, request):
+        return self.create(request)
+
+
+class RepairOrderDetail(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    serializer_class = RepairOrderSerializer
+    queryset = RepairOrder.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(self, request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(self, request, *args, **kwargs)
